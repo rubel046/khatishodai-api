3fc171c8490fe27724ba837b658c195116e6c0e3
Al-Amin <alamincse@outlook.com>
2020-05-31 21:32:59 +0600

    create a resource route
    resource route work for list, details, create, update and delete

--- app/Http/Helper/Constants.php
+++ app/Http/Helper/Constants.php
@@ -1,12 +1,21 @@
 <?php
 
-    define('SAVE_SUCCESS', 'Successfully Saved!');
-    define('UPDATE_SUCCESS', 'Successfully Updated!');
-    define('DELETE_SUCCESS','Successfully Deleted!');
-    define('NO_DATA','No data found!');
-    define('DATA_FOUND','Result with this query!');
-    define('ERROR_MSG','Error  occurred!');
-    define('PER_PAGE',5);
+define('SAVE_SUCCESS', 'Successfully Saved!');
+define('UPDATE_SUCCESS', 'Successfully Updated!');
+define('DELETE_SUCCESS', 'Successfully Deleted!');
+define('NO_DATA', 'No data found!');
+define('DATA_FOUND', 'Result with this query!');
+define('ERROR_MSG', 'Error  occurred!');
+define('PER_PAGE', 5);
 
+function resource($uri, $controller)
+{
+    $route = app('router');
+    $route->get($uri, $controller . '@index');
+    $route->post($uri, $controller . '@store');
+    $route->get($uri . '/{id}', $controller . '@show');
+    $route->put($uri . '/{id}', $controller . '@update');
+    $route->delete($uri . '/{id}', $controller . '@destroy');
+}
 
 ?>
