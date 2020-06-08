013a1c93bdb205e116503151e517e367ab2c9216
shamim-dev <mrezashamim@gmail.com>
2020-06-08 23:45:11 +0600

    Country, State/Division and city world data seeding

--- database/seeds/CityTableSeeder.php
+++ database/seeds/CityTableSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Model\Division;
 use Illuminate\Database\Seeder;
 use Faker\Factory as Faker;
 use Illuminate\Support\Facades\DB;
@@ -14,17 +15,33 @@ class CityTableSeeder extends Seeder
     public function run()
     {
         $faker = Faker::create();
-        foreach (range(1, 100) as $index) {
-            DB::table('cities')->insert([
-                'division_id' => $faker->numberBetween(1,100),
-                'name' => $faker->city,
-                'status' => $faker->randomElement([1,2,3]),
-                'created_by' => $faker->randomElement([1,2,3]),
-                'updated_by' => $faker->randomElement([1,2,3]),
-                'created_at' => $faker->dateTime,
-                'updated_at' => $faker->dateTime,
-                'ip_address' => $faker->ipv4,
-            ]);
-        }
+
+        Schema::disableForeignKeyConstraints();
+        DB::table('cities')->truncate();
+        $states = file_get_contents(
+            'https://raw.githubusercontent.com/hiiamrohit/Countries-States-Cities-database/master/states.json'
+        );
+        $states = collect(json_decode($states)->states);
+        $fileCities = file_get_contents(base_path('cities.json'));
+        $cities = collect(json_decode($fileCities));
+        $cities->map(function ($city) use ($states, $faker) {
+            $stateIdFinder = $states->firstWhere('id', '=', optional($city)->state_id);
+            $state = Division::whereName(optional($stateIdFinder)->name)->first();
+            if ($state) {
+                DB::table('cities')->insert([
+                    'name' => optional($city)->name,
+                    'division_id' => optional($state)->id,
+                    'status' => $faker->boolean,
+                    'created_by' => $faker->randomElement([1, 2, 3]),
+                    'updated_by' => $faker->randomElement([1, 2, 3]),
+                    'created_at' => $faker->dateTime,
+                    'updated_at' => $faker->dateTime,
+                    'ip_address' => $faker->ipv4,
+                ]);
+            }
+
+        });
+
+
     }
 }
