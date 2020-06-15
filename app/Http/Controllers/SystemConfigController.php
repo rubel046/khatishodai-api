e8d8f9c9cd4e6e5e11c86e2dac2edd3bfb19ef53
shamim-dev <mrezashamim@gmail.com>
2020-06-15 22:18:42 +0600

    system config

---
+++ app/Http/Controllers/SystemConfigController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Model\SystemConfig;
+use App\Repositories\Repository;
+use Illuminate\Http\Request;
+
+class SystemConfigController extends Controller
+{
+    private $model;
+
+    public function __construct(SystemConfig $model)
+    {
+        $this->middleware('auth');
+        $this->model = new Repository($model);
+    }
+
+    public function index()
+    {
+        return $this->model->all();
+    }
+
+
+    public function store(Request $request)
+    {
+        $this->validation($request);
+        return $this->model->create($request->all());
+    }
+
+
+    public function show($id)
+    {
+        return $this->model->show($id);
+    }
+
+
+    public function update(Request $request, $id)
+    {
+        $this->validation($request, $id);
+        return $this->model->update($request->all(), $id);
+    }
+
+
+    public function destroy($id)
+    {
+        return $this->model->delete($id);
+    }
+
+    private function validation(Request $request, $id = false)
+    {
+        $this->validate($request, [
+            'name' => 'required|string',
+            'alias' => 'required|string|unique:system_configs,alias' . ($id ? ', ' . $id : ''),
+            'purpose' => 'sometimes|string',
+            'data' => 'required|string',
+            'status' => 'numeric',
+        ]);
+    }
+
+}
---
+++ database/migrations/2020_06_15_153324_create_system_configs_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Http\Helper\CustomBlueprint;
+
+class CreateSystemConfigsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $schema = DB::connection()->getSchemaBuilder();
+        $schema->blueprintResolver(function ($table, $callback) {
+            return new CustomBlueprint($table, $callback);
+        });
+
+        $schema->create('system_configs', function (CustomBlueprint $table) {
+            $table->id();
+            $table->string('name',150);
+            $table->string('alias',100)->unique();
+            $table->string('purpose',150)->nullable();
+            $table->longText('data');
+            $table->commonFields();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('system_configs');
+    }
+}
